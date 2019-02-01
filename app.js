d990a6e6486d28d3366d4e9e549c49bafb093b60
Andy Prickett <andy@andyprickett.com>
2019-02-01 11:17:55 -0800

    Build server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// Settings
+app.set("port", normalizePort(process.env.PORT || "3000"));
+
+const server = http.createServer(app);
+server.listen(app.get("port"), () => {
+  console.log(`Server is listening for requests on port ${app.get("port")}...`);
+});
+
+/* ---------- */
+
+// Normalize Port for Server
+function normalizePort(value) {
+  // first
+  var port = parseInt(value, 10);
+  if (isNaN(port)) {
+    return value;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
